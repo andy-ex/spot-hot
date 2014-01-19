a40506152a964a68f9bc53d95727ead6844b3e8e
Dmitry <volchek.mail@gmail.com>
2014-01-19 22:14:33 +0300

    Sec commit

--- src/main/java/by/bsu/spothot/desktop/launch/Launcher.java
+++ src/main/java/by/bsu/spothot/desktop/launch/Launcher.java
@@ -4,6 +4,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -35,10 +38,40 @@ public class Launcher extends Application
         Parent rootNode = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
 
         log.debug("Showing JFX scene");
-        Scene scene = new Scene(rootNode, 400, 200);
+        Scene scene = new Scene(rootNode);
 
         stage.setTitle("Hello JavaFX and Maven");
         stage.setScene(scene);
         stage.show();
+
+        //Code to delete
+        FXMLLoader barLoader = new FXMLLoader();
+        GridPane progressBarParent = (GridPane) barLoader.load(getClass().getResourceAsStream("/fxml/progress-bar.fxml"));
+        VBox vBox = (VBox) progressBarParent.getChildren().get(0);
+        final ProgressBar progressBar = (ProgressBar) vBox.getChildren().get(0);
+
+        Scene bar =  new Scene(progressBarParent);
+
+        Stage barStage = new Stage();
+        barStage.setScene(bar);
+        barStage.show();
+
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                for (int i = 0; i <= 100; ++i)
+                {
+                    try {
+                        this.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    progressBar.setProgress(new Double(i) / 100.0);
+                }
+            }
+        };
+
+        thread.start();
+
     }
 }
